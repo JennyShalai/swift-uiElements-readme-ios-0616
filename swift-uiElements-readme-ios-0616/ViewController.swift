0ac103d8270c3e3283c6ca16907d91d1e743a9a8
JennyShalai <jenny.shalai@gmail.com>
2016-11-24 22:00:06 -0500

    add new project, ui is fun

---
+++ swift-uiElements-readme-ios-0616/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-uiElements-readme-ios-0616
+//
+//  Created by Flatiron School on 11/24/16.
+//  Copyright © 2016 jennyshalai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
